0281ee7f7f9edd1c403821bcd1d3e232579cfc47
seabassapologist <teamseabass@panfried.fish>
2024-01-13 14:41:21 -0500

    full width character lookup table

--- convert_to_markdown.py
+++ convert_to_markdown.py
@@ -2,7 +2,19 @@
 
 import sys
 
+print("| Index | Offset | Address   | Image |")
+print("|-------|--------|-----------|-------|")
+
+exclude = [0x24, 0x25, 0x29, 0x2A, 0x2B, 0x2C, 0x2F, 0x31, 0x32, 0x34, 0xED, 0xEE, 0xEF, 0xF3, 0xF4, 0xF8, 0xF9, 0xFA, 0xFB, 0xFC]
+
 with open(sys.argv[1]) as table:
+    index = 0x11
     for line in table:
         line = line.strip().split(" ")
-        print(f"| `${line[0]}` | `${line[2]}{line[1]}`|")
+        offset = f"{line[1]}{line[0]}"
+        if index not in exclude:
+            addr = str(hex(int(offset, 16) + 0x58000)).upper()
+            print(f"| `${str(hex(index)).upper()[2:]}` | `${line[1]}{line[0]}`| `$0{addr[2:]}` | ![](images/full_width_regular/0x{str(hex(index))[2:].upper()}.png)")
+        else:
+            print(f"| `${str(hex(index)).upper()[2:]}` | `${line[1]}{line[0]}`| `N/A` | ![](images/full_width_regular/blank.png)")
+        index+=0x01
---
+++ images/full_width_regular/rename_files.py
@@ -0,0 +1,16 @@
+import pathlib
+import natsort
+import os
+
+exclude = [0x24, 0x25, 0x29, 0x2A, 0x2B, 0x2C, 0x2F, 0x31, 0x32, 0x34, 0xED, 0xEE, 0xEF, 0xF3, 0xF4, 0xF8, 0xF9, 0xFA, 0xFB, 0xFC]
+
+h = 0x11
+# d = 0
+p = pathlib.Path('.')
+s = natsort.natsorted(list(p.glob('*.png')))
+print(s)
+for d in range(0,227):
+    if h not in exclude:
+        s[d].rename(f"0x{str(hex(h)).upper()[2:]}.png")
+        #print(f"{s[d].name} -> 0x{str(hex(h)).upper()[2:]}.png")
+    h += 0x01
