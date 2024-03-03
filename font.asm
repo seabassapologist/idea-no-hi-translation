960f3fab83b3b8e97b674f58d866551c5b984af2
seabassapologist <teamseabass@panfried.fish>
2024-03-03 14:23:05 -0500

    8x8 font patching

--- font.asm
+++ font.asm
@@ -1,4 +1,11 @@
 norom
+; patch the 8x8 lookup table to line up with the new 8x16 table
+org $058006
+incbin "8x8-table.bin"
+
+; import the 8x8 english font set
+org $058FA6
+incbin "glimmer-sans-8x8-1bpp.bin"
 
 ; patch the half-width lookup table to point to use 16 byte tiles
 org $0581E6
@@ -10,7 +17,7 @@ incbin "glimmer-sans-8x16-1bpp.bin"
 
 ; Junk to just test text printing, prints one of every 8x16 character
 org $060000
-db $02,$11,$12,$13,$14,$15,$16,$17,$18,$19,$1A,$1B,$1C,$1D,$1E
+db $01,$11,$12,$13,$14,$15,$16,$17,$18,$19,$1A,$1B,$1C,$1D,$1E
 db $1F,$20,$21,$22,$23,$24,$25,$26,$27,$28,$29,$0D,$2A,$2B,$2C
 db $2D,$2E,$2F,$30,$31,$32,$33,$34,$35,$36,$37,$38,$39,$3A,$3B
 db $3C,$3D,$3E,$3F,$40,$41,$42,$43,$0D,$44,$45,$0D,$46,$47,$48
--- misc/generate_table.py
+++ misc/generate_table.py
@@ -1,10 +1,12 @@
 output = ""
-offset = 0x12C2
+offset = 0x0FA6
 
-for i in range(0, 80):
+for i in range(0, 81):
     string = str(hex(offset))[2:].upper()
+    if len(string) == 3:
+        string = "0" + string
     le = f"{string[2:]} {string[:2]}"
     output += le + " "
-    offset += 0x10
+    offset += 0x08
 
 print(output)
