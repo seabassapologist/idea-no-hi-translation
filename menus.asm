14fa5ef1c67076f34b07be2c22326eec82c95195
seabassapologist <teamseabass@panfried.fish>
2024-04-06 11:49:00 -0400

    fix address to be PRG instead of lorom

--- menus.asm
+++ menus.asm
@@ -15,17 +15,17 @@ db $01,$02,$14,$C7,$00,$02,$02,$09
 
 ; --Wardrobe Screen--
 ; resize stat box to 14x7 tiles and shift right 3 tiles
-org $81F161
+org $00F161
 LDX #$0211
-org $81F166
+org $00F166
 LDX #$070E
 ; shift stat labels right 3 tiles
-org $81F16F
+org $00F16F
 LDX #$0412
-org $81F181
+org $00F185
 LDX #$0512
 ; orient the resistance labels to be horizontal and move them below the the stats
-org $81F1A2
+org $00F1A2
 LDA $190F
 STA $02
 LDA #$07
