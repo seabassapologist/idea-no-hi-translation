b3e9e9330b6fb5949c74ccec1864a58d2c42a8c1
seabassapologist <teamseabass@panfried.fish>
2024-09-07 13:32:26 -0400

    en table restructuring

--- convert_script.py
+++ convert_script.py
@@ -13,23 +13,24 @@ EN_TABLE = {}
 TEST_STRING = "[Full]Doctor \"Come now! Today's[Line] the day where you will[Line] show us your powers![Line] Do it now!\"[End]"
 
 def match_control_code(matchobj):
-    if matchobj.group(0) in EN_TABLE.keys():
+    if matchobj.group(0) in EN_TABLE["control_codes"]:
         return EN_TABLE[matchobj.group(0)]
     else:
         return "XX" # should never happen 
 
-def to_bytearray(eng_str):
+def to_bytearray(eng_str, table):
     byte_str = ""
     tokens = re.split(r"(\[\w+\])", eng_str)[1:-1]
     print(tokens)
     for token in tokens:
         # byte_str += re.sub(r"(\[\w+\])", match_control_code, token)
-        if token in EN_TABLE.keys():
-            byte_str +=  EN_TABLE[token]
+        if token in EN_TABLE["control_codes"]:
+            byte_str +=  EN_TABLE["control_codes"][token]
         else:
-            key_words = re.split(r"(\w+)", token)[1:-1]
-            print(key_words)
-            byte_str += token
+            # key_words = re.split(r"(\w+)", token)[1:-1]
+            tr_token = token.translate(table)
+            print(tr_token)
+            byte_str += tr_token
     print(byte_str)
     
 
@@ -44,13 +45,14 @@ def main():
         exit(1)
 
     try:
-        with open(TABLE_FILE, "r", encoding="utf8") as script:
+        with open(SCRIPT_FILE, "r", encoding="utf8") as script:
             script_file = json.load(script)
     except IOError as err:
         print(f"Can't open Script file '{SCRIPT_FILE}': {err}")
         exit(1)
 
-    to_bytearray(TEST_STRING)
+    trans_table = str.maketrans(EN_TABLE["characters"])
+    to_bytearray(TEST_STRING, trans_table)
 
 if __name__ == "__main__":
     main()
