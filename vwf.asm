4dceac855367760b728216fd9c82dc055b9b7675
seabassapologist <teamseabass@panfried.fish>
2025-05-18 17:05:08 -0400

    [Fixed] control code for font

--- vwf.asm
+++ vwf.asm
@@ -102,10 +102,33 @@ Reset_String_End:
     stz $1B02
     stz $1B03
     rtl
+; Logic to properly set the [Fixed] control code parameters
+Check_Fixed_Width:
+    cmp #$02            ; if $02 we found a [Fixed] control code
+    bne Not_Fixed
+    dec                 ; decrement so that font type flag is set to half-width
+    sta $1877           ; set the flag for fixed width
+    jmp Store_Results
+Not_Fixed:
+    stz $1877           ; make sure vwf flag is set to zero, in case font is switched mid-string
+Store_Results:
+    sta $1878           ; set the flag for font type
+    pla                 ; pull the address from earlier jsr off stack
+    pla                 ; hate this but i only had 4 bytes to work with
+    jml $8186EE         ; jump back to beginning of text loading routine
+; Check if we're doing fixed width or vwf 
+Check_VWF_Flag:
+    lda $1877           ; vwf flag - if 0 do vwf, if 1 do fixed-with
+    beq Do_VWF
+    jml $818EEA         ; fixed width routine
+Do_VWF:
+    jsl Set_Shift
+    jml $818DEA         ; vwf routine
+
 
 ; Initialize the bit shift parameters for the current character, and go right into the alternate Half-width routine every time
 org $818DE2
-jsl Set_Shift
+jml Check_VWF_Flag
 nop
 nop
 nop
@@ -160,3 +183,7 @@ jsl Store_Hero_Letter
 ; jump to the new string terminator handling code to reset bitshift variables
 org $818793
 jml Reset_String_End
+
+; jump to modified font control code handler
+org $8187B8
+jml Check_Fixed_Width
