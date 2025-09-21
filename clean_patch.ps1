862a4c1e8839925a70b3a4c089cf0a7391b5f79a
seabassapologist <teamseabass@panfried.fish>
2025-09-21 14:25:45 -0400

    convert menu asm to lorom addresses

--- clean_patch.ps1
+++ clean_patch.ps1
@@ -2,9 +2,12 @@ Remove-Item '.\Idea no Hi (Japan).sfc'
 Copy-Item -Path '.\Idea no Hi (Japan) - Clean.sfc' -Destination '.\Idea no Hi (Japan).sfc'
 Write-Output "Patching Menus..."
 .\asar.exe -v -werror code\menus.asm 'Idea no Hi (Japan).sfc'
+Write-Output ""
 Write-Output "Patching Font..."
 .\asar.exe -v -werror code\font.asm 'Idea no Hi (Japan).sfc'
+Write-Output ""
 Write-Output "Patching Variable Width Font..."
 .\asar.exe -v -werror code\vwf.asm 'Idea no Hi (Japan).sfc'
+Write-Output ""
 Write-Output "Inserting English Script"
 .\asar.exe -v -werror code\script.asm 'Idea no Hi (Japan).sfc'
--- misc/menus_norom.asm
+++ misc/menus_norom.asm
@@ -1,183 +1,188 @@
-lorom
+norom
 
 ; --Field Menu windows--
 ; shift the money window to the right by 1 tile
-org $81E6F2
+org $00E6F2
 ldx #$0215
-org $81E705
+org $00E705
 ldx #$0316
-org $81E716
+org $00E716
 ldx #$0318
+; switch the money window font type to [Fixed] to prevent border palette corruption when there's 6 digits
+org $00FA41
+lda #$02
+
 ; widen the main field menu window
-org $82C8FA
+org $0148FA
 db $01,$02,$11,$C7,$00,$02,$02,$06
+
 ; --Inventory Menus--
 ; shift the item actions menu window left 1 tile
-org $82C902
+org $014902
 db $01,$06,$08,$CB,$00,$01,$04,$00
 ; adjust the parameters used to preserve what's under the item actions window before it's drawn
-org $8280F5
+org $0100F5
 ldx #$0801
-org $8280FA
+org $0100FA
 ldx #$0409
 ; shift the inventory window left 7 tiles and widen to 30 tiles
-org $81E7FF
+org $00E7FF
 lda #$01
-org $81E818
+org $00E818
 lda #$1E
 ; adjust the parameters used to clear inventory window after closing
-org $8281AD
+org $0101AD
 ldx #$0601
-org $8281B2
+org $0101B2
 ldx #$0A1E
 ; preserve the bottom two row of tiles of the field menu window to prevent tile flickering when clearing the inventory window
-org $828098
+org $010098
 ldx #$0601
-org $82809D
+org $01009D
 ldx #$0214
 ; shift the inventory cursor left 7 tiles
-org $82D312
+org $015312
 ADC #$02
-org $82D2E8
+org $0152E8
 ldx #$0702
 ; shift the inventory text left 7 tiles when redrawing while scrolling
-org $82D026
+org $015026
 lda #$03
 ; widen the spacing between the two columns to 13 tiles
-org $81E90A
+org $00E90A
 lda #$0E
-org $82D1A5
+org $0151A5
 ADC #$0E
-org $82D0B5
+org $0150B5
 ADC #$0E
 ; adjust values needed to scroll inventory text properly
-org $82D062
+org $015062
 lda #$1E
-org $82D07F
+org $01507F
 ldx #$011E
-org $82D0FD
+org $0150FD
 lda #$1E
-org $82D13B
+org $01513B
 lda #$1E
-org $82D15B
+org $01515B
 ldx #$011E
-org $82D1E6
+org $0151E6
 lda #$1E
 ; shift the item description window left 1 tile and widen to 24 tiles
-org $82D338
+org $015338
 ldx #$0207
-org $81ED99
+org $00ED99
 ldx #$0418
 ; adjust the parameters for preserving the tiles that will be overwritten by the description window. This has to be done separately for all 4 item actions
 ; use
-org $828202
+org $010202
 ldx #$0207
-org $828207
+org $010207
 ldx #$040E
-org $828210
+org $010210
 ldx #$0215
 ; give
-org $8283D5
+org $0103D5
 ldx #$0207
-org $8283DA
+org $0103DA
 ldx #$040E
-org $8283E3
+org $0103E3
 ldx #$0215
 ; move
-org $8288CD
+org $0108CD
 ldx #$0207
-org $8288D2
+org $0108D2
 ldx #$040E
-org $8288DB
+org $0108DB
 ldx #$0215
 ; toss
-org $828AA3
+org $010AA3
 ldx #$0207
-org $828AA8
+org $010AA8
 ldx #$040E
-org $828AB1
+org $010AB1
 ldx #$0215
 ; relocate the "who?" window to the upper left corner of the screen
-org $82B47E
+org $01347E
 ldx #$0201
-org $82B48C
+org $01348C
 ldx #$0302 
-org $82B499
+org $013499
 ldx #$0402
 
 ; --Special Ability Windows--
 ; shift the ability window left 7 tiles and widen to 30 tiles
-org $81EAEA
+org $00EAEA
 lda #$01
-org $81EAFE
+org $00EAFE
 ldx #$0A1E
 ; adjust the parameters for clearing ability window after closing
-org $828CD3
+org $010CD3
 ldx #$0601
-org $828CD8
+org $010CD8
 ldx #$0A1E
-org $828C4E
+org $010C4E
 ldx #$0A01
-org $828C53
+org $010C53
 ldx #$0209
 ; widen the spacing between the two columns to 13 tiles
-org $81EB8A
+org $00EB8A
 lda #$0E
-; org $00EC90
+org $00EC90
 ; adjust the values used to scroll the text properly
-org $82D9DC
+org $0159DC
 lda #$03
-org $82DA81
+org $015A81
 ADC #$0E
-org $82DA0E
+org $015A0E
 lda #$1E
-org $82DAE8
+org $015AE8
 lda #$1E
-org $82DA2B
+org $015A2B
 ldx #$011E
-org $82DBB3
+org $015BB3
 ADC #$0E
-org $82DB26
+org $015B26
 lda #$1E
-org $82DBE9
+org $015BE9
 lda #$1E
-org $82DB46
+org $015B46
 ldx #$011E
 ; adjust the cursor positioning to line up with the new window and text spacing
-org $82DCF8
+org $015CF8
 lda #$02
-org $82DD76
+org $015D76
 ADC #$02
-org $82DD6D
+org $015D6D
 lda #$0E
 ; shift the ability description window left 1 tile
-org $82DD9B
+org $015D9B
 ldx #$0207
 ; adjust values used to restore tiles overwritten by description window. This has to be done separately for both options
 ; use
-org $828D28
+org $010D28
 ldx #$0207
-org $828D2D
+org $010D2D
 ldx #$040E
-org $828D36
+org $010D36
 ldx #$0215
 ; move
-org $828EF7
+org $010EF7
 ldx #$0207
-org $828EFC
+org $010EFC
 ldx #$040E
-; org $010F05
+org $010F05
 ; shift the ability actions window left 1 tile
-org $82C90A
+org $01490A
 db $01,$0A,$08,$CF,$00,$01,$02,$00
 ; adjust the values for restoring tiles overwritten by the ability actions menu
-org $828C01
+org $010C01
 ldx #$0601
-org $828C06
+org $010C06
 ldx #$0214
 
 ; --Wardrobe Screen--
-org $8BB36D ; $8BB36D
+org $05B36D ; $8BB36D
 Stat_Label_Line_Check:
 ;pha
 txa
@@ -209,33 +214,33 @@ rtl
 Select_Font_Type:
 lda $1B02
 cmp #$01
-bne Default_Font
+bne Default
 stz $18FB
 rtl
-Default_Font:
+Default:
 lda #$02
 sta $18FB
 rtl
 
 ; resize stat window to 14x7 tiles and shift right 3 tiles
-org $81F161
+org $00F161
 ldx #$050D
-org $81F166
+org $00F166
 ldx #$0412
 ; shift stat labels right 3 tiles
-org $81F16F
+org $00F16F
 ldx #$060E
-org $81F185
+org $00F185
 ldx #$070E
 ; orient the resistance labels to be horizontal and move them below the the stats
-org $81F19B
+org $00F19B
 lda #$19
-org $81F1A2
+org $00F1A2
 lda $190F
 sta $02
-jsl Stat_Label_Line_Check
+jsl $8BB36D
 ; make the resistance labels print 3 tiles apart
-org $81F1BA
+org $00F1BA
 stz $07
 jsl $81FC0D
 inc $190F
@@ -244,132 +249,132 @@ inc $190F
 inx
 txa
 ; change text palette for stat values to orange might not keep this
-org $81F1FA 
+org $00F1FA 
 lda #$00
 ; make stat values print as 8x8 characters
-org $81F1FF
-jsl Patch_Font_Type
-org $81F22A
-jsl Patch_Font_Type
-org $81F255
-jsl Patch_Font_Type
-org $81F280
-jsl Patch_Font_Type
+org $00F1FF
+jsl $8BB38A
+org $00F22A
+jsl $8BB38A
+org $00F255
+jsl $8BB38A
+org $00F280
+jsl $8BB38A
 
-org $81FA41
-jsl Select_Font_Type
+org $00FA41
+jsl $8BB399
 nop
 ; lda #$00
 ; org $00FB36
 ; jsl $8BB38A
 ; shift the stat values right 4 tiles (pow, def, speed, luck respectively)
-org $81F1D1
+org $00F1D1
 lda #$0610
-org $81F205
+org $00F205
 lda #$0615
-org $81F230
+org $00F230
 lda #$0710
-org $81F25B
+org $00F25B
 lda #$0715
 ; place the resistance icons 1 tile to the right of their label (heat, cold, electric, mind respectively)
-org $81F2A7
+org $00F2A7
 ldx #$061A
-org $81F2CE
+org $00F2CE
 ldx #$061D
-org $81F2FA
+org $00F2FA
 ldx #$071A
-org $81F322
+org $00F322
 ldx #$071D
 ; update the character bytes for resistance symbols
-org $81F340
+org $00F340
 db $69,$10,$68,$6A
 ; change the character bytes for the resistance labels
-org $81F33B
+org $00F33B
 db $6F,$70,$71,$72
 ; resize clothing list to 12x12 tiles, shift right by 1 tile, and shift down 1 tile
-org $81EF04
+org $00EF04
 ldx #$090D
-org $81EF09
+org $00EF09
 ldx #$1212
 ; shift clothing labels left 1 tile
-org $81EF17
+org $00EF17
 lda #$0E
 ; shift all clothing labels down by 1 tile
-org $81EFCB
+org $00EFCB
 db $0A,$A7,$00,$0C,$A8,$00,$0E,$A9,$00,$10,$AA,$00,$12,$AB,$00,$14,$AC,$00,$16,$AD,$00,$18,$AE,$00
 ; shift clothing text down by 1 tile and to the left by 3 tiles
-org $81EF8D
+org $00EF8D
 lda #$0A
-org $81EFA6
+org $00EFA6
 lda #$10
 ; widen the wardrobe actions menu to 16 tiles and shift left by 1 tile
-org $82C912
+org $014912
 db $04,$01,$18,$D1,$00,$03,$01,$07
 ; reposition the selection arrow for the clothing list
-org $8295AE
+org $0115AE
 lda #$0D
-org $8295B3
+org $0115B3
 lda #$0A
 ; change some magic value to get the bottom border of the clothing list to draw
-org $829150
+org $011150
 ldx #$1902
 ; shift the window for selecting clothes from inventory left by 1 tile and widen to 16 tiles
-org $8296FC
+org $0116FC
 ldx #$0601
-org $829701
+org $011701
 ldx #$1410
 ; shift the clothing in inventory text left by 2 tiles
-org $82980E
+org $01180E
 lda #$02
 ; shift the clothing in inventory arrow left by 2 tiles
-org $829886
+org $011886
 ldx #$0701
-org $829A22
+org $011A22
 lda #$01
 ; shift the clothing swap text left by 2 tiles
-org $82972B
+org $01172B
 lda #$02
 ; adjust parameters to preserve the "who?" box before transitioning to the wardrobe screen
-org $82910F
+org $01110F
 ldx #$0101
-org $829114
+org $011114
 ldx #$0506
 
 ; --File Select Screen--
 ; shift options window left 2 tiles and widen to 
-org $86F478
+org $037478
 ldx #$0202
-org $86F47D
+org $03747D
 ldx #$041C
-org $86F48B
+org $03748B
 ldx #$0304
 ; TODO - Probably needs a custom routine to move the cursor in variable amounts
 ; shift cursor starting position for option window left 2 tiles
-org $86EB39
+org $036B39
 lda #$03
 ; increase cursor horizontal movement to 7 tiles
-org $86EB4D
+org $036B4D
 lda #$07
 
 ; --New Game Screens--
 ; update name input routine to allow 8 characters
-org $86EEC0
+org $036EC0
 cpy #$0008
 ; insert the modified naming screen table
-org $86F88E
+org $03788E
 incbin "../data/name-screen-table.bin"
 ; change the font type for in-progress name to [Fixed] so that overwriting the asterisks works correctly
-org $86F063
+org $037063
 ldx #$0102
 ; shift the in-progress name left 3 tiles
-org $86F058
+org $037058
 ldx #$0311
-org $86F068
+org $037068
 ldx #$0311
 ; widen the name window to 22 tiles and shift left by 1
-org $86EDDF
+org $036DDF
 ldx #$0205
-org $86EDE4
+org $036DE4
 ldx #$0416
-org $86EE0C
+org $036E0C
 ldx #$0307
