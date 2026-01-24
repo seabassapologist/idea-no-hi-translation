94daa9fe8af1558c0451d3efd0b609b46c7f0210
seabassapologist <teamseabass@panfried.fish>
2026-01-24 16:07:11 -0500

    more pascal string table builder stuff

--- tools/build_pascal_strings.py
+++ tools/build_pascal_strings.py
@@ -3,6 +3,7 @@
 # compile a lookup table and string data for pascal strings
 
 import json
+import struct
 
 STRINGS_FILE="pascal_strings.json"
 
@@ -14,8 +15,14 @@ def build_data(p_strings):
     return bytearray.fromhex(byte_str.strip())
 
 def build_table(p_strings):
-    byte_str = ""
-    byte_count = 0x0
+    table = b'\x00\x00'
+    offset = 0
+
+    for p_str in p_strings.values():
+        offset += int(p_str["length"]) + 1
+        table += struct.pack("<h", offset)       # '<h' = 16-bit little-endian
+
+    return table
 
 def main():
     try:
@@ -25,8 +32,20 @@ def main():
         print(f"Can't open Table file '{STRINGS_FILE}': {err}")
         exit(1)
 
-    pascal_data = build_data(pascal_strings)
-    print(pascal_data)
+    try:
+        with open("../data/pascal_string_data.bin", "bw") as pstrings_bin:
+            pstrings_bin.write(build_data(pascal_strings))
+    except IOError as err:
+        print(f"Failed to write pascal string data': {err}")
+        exit(1)
+
+    try:
+        with open("../data/pascal_string_table.bin", "bw") as pstrings_bin:
+            pstrings_bin.write(build_table(pascal_strings))
+    except IOError as err:
+        print(f"Failed to write pascal string table': {err}")
+        exit(1)
+
 
 if __name__ == "__main__":
     main()
